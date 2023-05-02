d0ed104660677026c42d51f53cd4dcd0ec614e97
Liam O <lokanedeveloper@gmail.com>
2023-05-02 17:42:09 +1000

    update contact page to include a form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,35 @@
-import React from "react";
-import Image from "../assets/images/liamo_headshot_01.JPG";
+import React, { useRef } from "react";
 import "../style/Contact.css";
 import NavBar from "./NavBar";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_okzh98r",
+        "template_r0q94mh",
+        form.current,
+        "deLSH2AXBRidmQvkU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <NavBar />
-      <div className="contactContainer">
+      {/* <div className="contactContainer">
         <div className="row">
           <div id="Contact" className="contactCollection">
             <div className="column-2">
@@ -21,7 +43,16 @@ export default function Contact() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      <form className="formContainer" ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input className="inputSubmit" type="submit" value="Send" />
+      </form>
     </>
   );
 }
